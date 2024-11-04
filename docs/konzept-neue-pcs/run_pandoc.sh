a7f1f05fd905352a040155ec4ec2f4e299588ba5
Andreas Roth <andreas@batchworks.de>
2024-11-04 23:08:56 +0100

    test build of docs

--- docs/konzept-neue-pcs/run_pandoc.sh
+++ docs/konzept-neue-pcs/run_pandoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker run -ti --rm \
-       --volume "$(pwd):/app" \
-       schose/pandoc_mac:latest pandoc konzept.de.md -f markdown -o konzept.de.pdf --toc --pdf-engine=xelatex --variable titlepage=true -F mermaid-filter --template  https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/v2.0.0/eisvogel.tex --listings
+docker run --rm \
+        --volume "$(pwd):/workdir" \
+        schose/pandoc:latest pandoc konzept.de.md -f markdown -o konzept.de.pdf --toc --pdf-engine=xelatex --variable titlepage=true -F mermaid-filter --template  https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/v2.0.0/eisvogel.tex --listings
