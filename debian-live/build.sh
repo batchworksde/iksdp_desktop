1d44e3ff02035d50b8b469fe03d0619743a3976c
birkb <birk@batchworks.de>
2024-11-10 17:49:34 +0100

    use version info for live hostname - to make it easy to check which version is currently used

--- debian-live/build.sh
+++ debian-live/build.sh
@@ -81,7 +81,7 @@ function configImage {
     --chroot-squashfs-compression-type "${DEBIAN_SQUASHFS_COMPRESSION_TYPE}" \
     --compression xz \
     --archive-areas "main non-free-firmware" \
-    --bootappend-live "boot=live config locales=${DEBIAN_LOCALES} keyboard-layouts=${DEBIAN_KEYBOARD_LAYOUTS} timezone=${DEBIAN_TIMEZONE} ${liveConfigOptions}" \
+    --bootappend-live "boot=live config hostname=iksdp-${RELEASE_VERSION} locales=${DEBIAN_LOCALES} keyboard-layouts=${DEBIAN_KEYBOARD_LAYOUTS} timezone=${DEBIAN_TIMEZONE} ${liveConfigOptions}" \
     --image-name debian-live-"${DEBIAN_VERSION}"-"${RELEASE_VERSION}"-"${IMAGE_TIMESTAMP}"
   if [ "$?" -ne 0 ]; then
     logerror "${FUNCNAME[0]}" "Debian image configuration failed"
