c988760ec93986546b58d2639aea98c1ab22d4ae
birkb <birk@batchworks.de>
2024-12-08 00:41:43 +0100

    macOS support added - run docker without sudo because the docker group already provides root perms - sed function to support non GNU sed version of macOS - some (formatting) changes to make shellcheck happier

--- debian-live/container-build.sh
+++ debian-live/container-build.sh
@@ -4,11 +4,10 @@ set -e
 set -u
 set -o pipefail
 
-
 declare CONTAINERCOMMAND
 
 function showHelp {
-  echo -e "Usage: $(basename $0) [OPTION]"
+  echo -e "Usage: $(basename "${0}") [OPTION]"
   echo -e ""
   echo -e "This script is supposed to be run from the root directory."
   echo -e "Running the script without any options starts the building process of the iksdp image."
@@ -24,36 +23,40 @@ function showHelp {
 }
 
 function checkContainerCommand {
-  if [ $(command -v podman) 2>&1>/dev/null ]; then
-    CONTAINERCOMMAND=podman
-  else
+  if [ -x "$(command -v podman)" ]; then
+    CONTAINERCOMMAND="sudo podman"
+  elif [ -x "$(command -v docker)" ]; then
     CONTAINERCOMMAND=docker
+  else
+    echo "No podman or docker command found"
+    exit 1
   fi
 }
 
 function buildBootstrapImage {
-  local useAptCacheAnswer=""
-  local useAptCache=true
-  local aptCacheUrlAnswer=""
-  local aptCacheUrl="http://localhost:3142"
-  local containerfileTemplatePath="$(pwd)/debian-live/Containerfile.template"
-  local containerfilePath="$(pwd)/debian-live/Containerfile"
-  local imageOptions=""
+  local useAptCacheAnswer useAptCache aptCacheUrlAnswer aptCacheUrl containerfileTemplatePath containerfilePath imageOptions
+  useAptCacheAnswer=""
+  useAptCache=true
+  aptCacheUrlAnswer=""
+  aptCacheUrl="http://localhost:3142"
+  containerfileTemplatePath="$(pwd)/debian-live/Containerfile.template"
+  containerfilePath="$(pwd)/debian-live/Containerfile"
+  imageOptions=""
 
   if [ ! -f "${containerfileTemplatePath}" ]; then
     echo "Containerfile.template not found at ${containerfileTemplatePath}."
     exit 1
   fi
 
-  read -n 1 -p "Use apt caching proxy (highly recommended)? [Y|n]: " useAptCacheAnswer
+  read -r -n 1 -p "Use apt caching proxy (highly recommended)? [Y|n]: " useAptCacheAnswer
   case "${useAptCacheAnswer,,}" in #${,,} converts to lower case
-    ""|"y")
-      read -p $'\n'"Apt caching proxy URL [http://localhost:3142]: " aptCacheUrlAnswer
+    "" | "y")
+      read -r -p $'\n'"Apt caching proxy URL [http://localhost:3142]: " aptCacheUrlAnswer
       if [ "${aptCacheUrlAnswer}" ]; then
         aptCacheUrl="${aptCacheUrlAnswer}"
       fi
       ;;
-    "n"|"N")
+    "n" | "N")
       useAptCache=false
       ;;
     *)
@@ -70,14 +73,14 @@ function buildBootstrapImage {
 
   if [ "$useAptCache" = true ]; then
     imageOptions="RUN echo 'Acquire::http { Proxy \"${aptCacheUrl}\"; };' > /etc/apt/apt.conf.d/51acng"
-    sed "s|{{ IMAGE_OPTIONS }}|${imageOptions}|" -i "${containerfilePath}"
+    patchContainerFile "s|{{ IMAGE_OPTIONS }}|${imageOptions}|" "${containerfilePath}"
   else
-    sed "s|{{ IMAGE_OPTIONS }}||" -i "${containerfilePath}"
+    patchContainerFile "s|{{ IMAGE_OPTIONS }}||" "${containerfilePath}"
   fi
 
   source debian-live/build.env
 
-  sudo $CONTAINERCOMMAND build \
+  $CONTAINERCOMMAND build \
     --build-arg debian_version="${DEBIAN_VERSION}" \
     --network host \
     -t iksdp_desktop-builder \
@@ -85,15 +88,29 @@ function buildBootstrapImage {
     debian-live
 }
 
+function patchContainerFile {
+  local patch file
+  patch="${1}"
+  file="${2}"
+
+  case "$OSTYPE" in
+    darwin*)
+      sed -i '' "${patch}" "${file}"
+      ;;
+    *)
+      sed -i "${patch}" "${file}"
+      ;;
+  esac
+}
+
 function runBootstrapContainer {
-  sudo $CONTAINERCOMMAND run \
+  $CONTAINERCOMMAND run \
     --rm \
     --network host \
     --privileged \
-    --volume $(pwd):/iksdp_desktop \
+    --volume "$(pwd)":/iksdp_desktop \
     iksdp_desktop-builder:latest
 }
-
 
 checkContainerCommand
 
@@ -102,17 +119,17 @@ if [ "$#" -eq 0 ]; then
 fi
 
 case "$@" in
-  -a|--all)
+  -a | --all)
     buildBootstrapImage
     runBootstrapContainer
     ;;
-  -b|--build)
+  -b | --build)
     buildBootstrapImage
     ;;
-  -h|--help)
+  -h | --help)
     showHelp
     ;;
-  -r|--run)
+  -r | --run)
     runBootstrapContainer
     ;;
 esac
