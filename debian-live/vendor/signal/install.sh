2a6acccd3c1d6defb5993aefac0e0e70eaf1a440
Andreas Roth <andrea@batchworks.de>
2025-01-03 00:37:43 +0100

    added time and powersettings

--- debian-live/vendor/signal/install.sh
+++ debian-live/vendor/signal/install.sh
@@ -1,3 +1,5 @@
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-cp "${SCRIPT_DIR}"/signal_install_in_chroot.hook.chroot "${BUILD_DIR}"/config/hooks/normal/
+if [[ "$DEBIAN_ARCH" == "amd64" ]]; then
+    SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+    cp "${SCRIPT_DIR}"/signal_install_in_chroot.hook.chroot "${BUILD_DIR}"/config/hooks/normal/
+fi
