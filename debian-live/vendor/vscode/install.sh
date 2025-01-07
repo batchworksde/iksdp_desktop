d0c9b213bb75cdc45c8399c0a49784c6be0f4142
Andreas Roth <andreas@batchworks.de>
2025-01-07 14:20:02 +0100

    update build-image.yml to include vendor

--- debian-live/vendor/vscode/install.sh
+++ debian-live/vendor/vscode/install.sh
@@ -1,3 +1,6 @@
+
+loginfo "fetchExternalPackages" "vscode package vendor package started"
+
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 cp "${SCRIPT_DIR}"/vscode_install_in_chroot.hook.chroot "${BUILD_DIR}"/config/hooks/normal/
