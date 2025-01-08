1eb829292e5cebcfd09ca3767493a4c6416ed186
Johannes Krausmueller <johannes@krausmueller.de>
2025-01-08 23:58:30 +0100

    use long form --continue-at instead of -C

--- debian-live/vendor/edge/install.sh
+++ debian-live/vendor/edge/install.sh
@@ -10,7 +10,7 @@ if [[ "$DEBIAN_ARCH" == "amd64" ]]; then
 
     # other way - not fine but working.. 
     loginfo "fetchExternalPackages" "egde package download started"
-    curl -C - --silent --location https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.99-1_amd64.deb --output "${BUILD_DIR}"/config/packages.chroot/microsoft-edge-stable_131.0.2903.99-1_amd64.deb
+    curl --continue-at - --silent --location https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.99-1_amd64.deb --output "${BUILD_DIR}"/config/packages.chroot/microsoft-edge-stable_131.0.2903.99-1_amd64.deb
     if [ "$?" -ne 0 ]; then
         logerror "fetchExternalPackages" "egde package download failed"
         exit 1
