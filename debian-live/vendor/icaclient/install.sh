00be404bc5e9a78e2cae3e1babc34fab660fb41b
Andreas Roth <andreas@batchworks.de>
2025-01-13 18:41:27 +0100

    updated icaclient to 24.11.0.85

--- debian-live/vendor/icaclient/install.sh
+++ debian-live/vendor/icaclient/install.sh
@@ -1,6 +1,6 @@
 loginfo "fetchExternalPackages" "icaclient package download started"
 
-curl --continue-at - --silent --location http://iksdp.pfadfinderzentrum.org/icaclient_24.8.0.98_"${DEBIAN_ARCH}".deb --output "${BUILD_DIR}"/config/packages.chroot/icaclient_24.8.0.98_"${DEBIAN_ARCH}".deb
+curl --continue-at - --silent --location http://iksdp.pfadfinderzentrum.org/icaclient_24.11.0.85_"${DEBIAN_ARCH}".deb --output "${BUILD_DIR}"/config/packages.chroot/icaclient_24.11.0.85_"${DEBIAN_ARCH}".deb
 if [ "$?" -ne 0 ]; then
   logerror "fetchExternalPackages" "icaclient package download failed"
   exit 1
