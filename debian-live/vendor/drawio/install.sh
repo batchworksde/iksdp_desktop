2b05b4e99a6739d34e3d73b4881bbaf162c1b66b
Andreas Roth <andreas@batchworks.de>
2025-01-13 11:30:36 +0100

    added drawio

---
+++ debian-live/vendor/drawio/install.sh
@@ -0,0 +1,14 @@
+loginfo "fetchExternalPackages" "drawio package vendor package started"
+
+if [ "${DEBIAN_ARCH}" = "amd64" ]; then
+    loginfo "fetchExternalPackages" "drawio package download started"
+    curl --continue-at - --silent --location url -s https://api.github.com/repos/jgraph/drawio-desktop/releases/latest | grep browser_download_url | grep '\.deb' | cut -d '"' -f 4 | grep -i ${DEBIAN_ARCH} | wget --no-verbose --output-document="${BUILD_DIR}"/config/packages.chroot/drawio-amd64-26.0.4.deb -i - 
+
+    if [ "$?" -ne 0 ]; then
+      logerror "fetchExternalPackages" "drawio package download failed"
+      exit 1
+    fi
+    loginfo "fetchExternalPackages" "drawio package download done"
+else
+    loginfo "fetchExternalPackages" "drawio package vendor package skipped"
+fi
