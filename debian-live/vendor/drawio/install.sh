8d726a2ab4b3c8b5c0939a877d5d271ffa614ecc
Andreas Roth <andreas@batchworks.de>
2025-01-23 20:08:27 +0100

    added torbrowser,fixed kubectl, drawio

--- debian-live/vendor/drawio/install.sh
+++ debian-live/vendor/drawio/install.sh
@@ -2,7 +2,7 @@ loginfo "fetchExternalPackages" "drawio package vendor package started"
 
 if [ "${DEBIAN_ARCH}" = "amd64" ]; then
     loginfo "fetchExternalPackages" "drawio package download started"
-    curl --continue-at - --silent --location url -s https://api.github.com/repos/jgraph/drawio-desktop/releases/latest | grep browser_download_url | grep '\.deb' | cut -d '"' -f 4 | grep -i ${DEBIAN_ARCH} | wget --no-verbose --output-document="${BUILD_DIR}"/config/packages.chroot/drawio-amd64-26.0.4.deb -i - 
+    curl --continue-at - --silent --location url -s https://api.github.com/repos/jgraph/drawio-desktop/releases/latest | grep browser_download_url | grep '\.deb' | cut -d '"' -f 4 | grep -i ${DEBIAN_ARCH} | wget --no-verbose --output-document="${BUILD_DIR}"/config/packages.chroot/drawio_amd64.deb -i - 
 
     if [ "$?" -ne 0 ]; then
       logerror "fetchExternalPackages" "drawio package download failed"
---
+++ debian-live/vendor/torbrowser/install.sh
@@ -0,0 +1,32 @@
+loginfo "fetchExternalPackages" "torbrowser package vendor package started"
+
+if [ "${DEBIAN_ARCH}" = "amd64" ]; then
+    loginfo "fetchExternalPackages" "torbrowser package download started"
+
+    curl --silent --location https://dist.torproject.org/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz --output "tor-browser.tar.xz"
+    
+    tar xf tor-browser.tar.xz -C ${BUILD_DIR}"/config/includes.chroot/opt/"
+    chmod -R 755 ${BUILD_DIR}"/config/includes.chroot/opt/tor-browser"
+
+    SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+    mkdir -p "${BUILD_DIR}"/config/includes.chroot/opt/iksdp/bin/
+    cp "${SCRIPT_DIR}"/start_tor_browser.sh "${BUILD_DIR}"/config/includes.chroot/opt/iksdp/bin/
+
+    loginfo "fetchExternalPackages" "torbrowser package download finished"
+
+
+    cat > ${BUILD_DIR}"/config/includes.chroot/usr/share/applications/tor-browser.desktop" <<EOL
+[Desktop Entry]
+Type=Application
+Name=Tor Browser
+GenericName=Web Browser
+Comment=Tor Browser  is +1 for privacy and −1 for mass surveillance
+Categories=Network;WebBrowser;Security;
+Exec=sh -c "/opt/iksdp/bin/start_tor_browser.sh"
+Icon=/opt/tor-browser/Browser/browser/chrome/icons/default/default128.png
+StartupNotify=true
+StartupWMClass=Tor Browser
+EOL
+fi
+
+
