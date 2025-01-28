ef3442695ff5be875cf931affbd57427aa9c5663
Johannes Krausmueller <johannes@krausmueller.de>
2025-01-29 00:01:20 +0100

    move download of torbrowser to cache - archive was downloaded to the root directory of the repo - use {BUILD_DIR}/cache/ instead and don't download every time (`--continue-at`)

--- debian-live/vendor/torbrowser/install.sh
+++ debian-live/vendor/torbrowser/install.sh
@@ -3,9 +3,9 @@ loginfo "fetchExternalPackages" "torbrowser package vendor package started"
 if [ "${DEBIAN_ARCH}" = "amd64" ]; then
     loginfo "fetchExternalPackages" "torbrowser package download started"
 
-    curl --silent --location https://dist.torproject.org/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz --output "tor-browser.tar.xz"
+    curl --continue-at - --silent --location https://dist.torproject.org/torbrowser/14.0.4/tor-browser-linux-x86_64-14.0.4.tar.xz --output "${BUILD_DIR}"/cache/tor-browser.tar.xz
     
-    tar xf tor-browser.tar.xz -C ${BUILD_DIR}"/config/includes.chroot/opt/"
+    tar xf "${BUILD_DIR}"/cache/tor-browser.tar.xz -C ${BUILD_DIR}"/config/includes.chroot/opt/"
     chmod -R 755 ${BUILD_DIR}"/config/includes.chroot/opt/tor-browser"
 
     SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
