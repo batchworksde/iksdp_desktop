000fe49b60dd6c2c076513e37587137a36e87c2a
Johannes Krausmueller <johannes@krausmueller.de>
2025-02-26 18:11:29 +0100

    fixes for vendor install scripts (fixes #90)
    - removed "--continue-at -"
    - additional error handling in install scripts
    - use latest version of torbrowser

--- debian-live/vendor/edge/install.sh
+++ debian-live/vendor/edge/install.sh
@@ -3,6 +3,10 @@ loginfo "fetchExternalPackages" "edge package vendor package started"
 if [ "${DEBIAN_ARCH}" = "amd64" ]; then
     SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
     cp "${SCRIPT_DIR}"/edge_install_in_chroot.hook.chroot "${BUILD_DIR}"/config/hooks/normal/
+    if [ "$?" -ne 0 ]; then
+        logerror "fetchExternalPackages" "copy of edge hook failed"
+        exit 1
+    fi
 else
     loginfo "fetchExternalPackages" "edge package vendor package skipped"
 fi
