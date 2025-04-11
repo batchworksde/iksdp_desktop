87a89d780f50879769e522a0888b267871abe674
Andreas Roth <andreas@batchworks.de>
2025-04-11 19:24:34 +0200

    test upload1

--- debian-live/build.sh
+++ debian-live/build.sh
@@ -636,6 +636,8 @@ function uploadIso {
 
 function uploadIsoS3 {
   loginfo "${FUNCNAME[0]}" "Upload the ISO to amazon s3"
+ 
+  snap install aws-cli --classic
 
   aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
   aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY }
