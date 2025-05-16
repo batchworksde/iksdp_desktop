2e8919cdab9ee3b6b96c9e9affd0721d0516c8cc
Johannes Krausmueller <johannes@krausmueller.de>
2025-05-16 11:55:56 +0200

    do not compress tar - only used when building DEBIAN_BINARY_IMAGE="tar" - https://manpages.debian.org/unstable/live-build/lb_config.1.en.html#compression)

--- debian-live/build.sh
+++ debian-live/build.sh
@@ -136,7 +136,7 @@ function configImage {
     --binary-image "${DEBIAN_BINARY_IMAGE}" \
     --bootloaders "${DEBIAN_BOOTLOADERS}" \
     --binary-filesystem "${DEBIAN_BINARY_FILESYSTEM}" \
-    --compression xz \
+    --compression "${DEBIAN_TAR_COMPRESSION_TYPE}" \
     --archive-areas "main non-free-firmware" \
     --bootappend-live "boot=live config hostname="${DEBIAN_HOSTNAME}" locales=${DEBIAN_LOCALES} keyboard-layouts=${DEBIAN_KEYBOARD_LAYOUTS} timezone=${DEBIAN_TIMEZONE} username=${DEBIAN_USERNAME} ${liveConfigOptions}" \
     --image-name debian-live-"${DEBIAN_VERSION}"-"${RELEASE_VERSION}"-"${IMAGE_TIMESTAMP}"
