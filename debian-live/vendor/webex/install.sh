00acc4fc5c8b44d10b9c8389d2d017eb6f372dc4
Andreas Roth <andreas@batchworks.de>
2025-06-16 19:44:45 +0200

    fixed webex

--- debian-live/vendor/webex/install.sh
+++ debian-live/vendor/webex/install.sh
@@ -1,6 +1,6 @@
 loginfo "fetchExternalPackages" "webex package download started"
 
-curl --silent --location https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb --output "${BUILD_DIR}"/config/packages.chroot/webex.deb
+curl --silent --location https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb --output "${BUILD_DIR}"/config/packages.chroot/webex_amd64.deb
 if [ "$?" -ne 0 ]; then
   logerror "fetchExternalPackages" "webex package download failed"
   exit 1
