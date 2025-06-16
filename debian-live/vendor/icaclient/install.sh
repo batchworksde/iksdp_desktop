a244577781653a8621c6103071ddbf9d664759f1
Andreas Roth <andreas@batchworks.de>
2025-06-16 11:04:31 +0200

    updated ica client

--- debian-live/vendor/icaclient/install.sh
+++ debian-live/vendor/icaclient/install.sh
@@ -1,6 +1,6 @@
 loginfo "fetchExternalPackages" "icaclient package download started"
 
-curl --silent --location https://iksdplinux.s3.amazonaws.com/icaclient_25.03.0.66_"${DEBIAN_ARCH}".deb --output "${BUILD_DIR}"/config/packages.chroot/icaclient_25.03.0.66_"${DEBIAN_ARCH}".deb
+curl --silent --location https://iksdplinux.s3.amazonaws.com/icaclient_25.05.0.44_"${DEBIAN_ARCH}".deb --output "${BUILD_DIR}"/config/packages.chroot/icaclient_25.03.0.66_"${DEBIAN_ARCH}".deb
 if [ "$?" -ne 0 ]; then
   logerror "fetchExternalPackages" "icaclient package download failed"
   exit 1
