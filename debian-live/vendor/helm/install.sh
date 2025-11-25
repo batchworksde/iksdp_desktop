2317d4a463b4c7bb56f7bbc37c8fc83e0420b117
Andreas Roth <andreas@batchworks.de>
2025-11-25 15:08:48 +0100

    added helm and more packages

---
+++ debian-live/vendor/helm/install.sh
@@ -0,0 +1,8 @@
+loginfo "fetchExternalPackages" "helm package vendor package started"
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+cp "${SCRIPT_DIR}"/helm_install_in_chroot.hook.chroot "${BUILD_DIR}"/config/hooks/normal/
+if [ "$?" -ne 0 ]; then
+    logerror "fetchExternalPackages" "copy of vscode hook failed"
+    exit 1
+fi
